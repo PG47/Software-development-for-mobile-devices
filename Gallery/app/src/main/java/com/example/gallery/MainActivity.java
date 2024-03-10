3170581f4da5cc7a88205034198f8c6fff62f63f
PG47 <86549123+PG47@users.noreply.github.com>
2024-03-11 00:25:56 +0700

    Setup Gallery permission

--- Gallery/app/src/main/java/com/example/gallery/MainActivity.java
+++ Gallery/app/src/main/java/com/example/gallery/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.gallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.Manifest;
+
+import java.util.Objects;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private static final int REQUEST_PERMISSIONS = 1234;
+    private static final String [] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    private static final int PERMISSION_COUNT = 2;
+
+    @SuppressLint("NewApi")
+    private boolean Permission_denied() {
+        for(int i=0; i < PERMISSION_COUNT; i++) {
+            if(checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED)
+            return true;
+        }
+        return false;
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, final String[] Permissions, final int[] results) {
+        super.onRequestPermissionsResult(requestCode,Permissions,results);
+        if(requestCode == REQUEST_PERMISSIONS && results.length > 0) {
+            if(Permission_denied()) {
+                ((ActivityManager) Objects.requireNonNull(this.getSystemService(ACTIVITY_SERVICE)))
+                        .clearApplicationUserData();
+                recreate();
+            } else {
+                onResume();
+            }
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Permission_denied()) {
+            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+            return;
+        }
     }
 }
