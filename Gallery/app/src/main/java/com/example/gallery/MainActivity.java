c8fd9179952a6065ca52bb90326b6ba250df9425
RoyalMike <bibo200311@gmail.com>
2024-03-11 15:22:11 +0700

    display images on listview

--- Gallery/app/src/main/java/com/example/gallery/MainActivity.java
+++ Gallery/app/src/main/java/com/example/gallery/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gallery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,12 +9,25 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.Manifest;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean Permission_denied() {
         for(int i=0; i < PERMISSION_COUNT; i++) {
             if(checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED)
-            return true;
+                return true;
         }
         return false;
     }
@@ -51,12 +65,81 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    private boolean isGalleryInitialized;
+
     @Override
     protected void onResume() {
         super.onResume();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Permission_denied()) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Permission_denied()) {
             requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
             return;
+        }
+
+        if (!isGalleryInitialized) {
+            final ListView listView = findViewById(R.id.Pic_list);
+            final GalleryAdapter galleryAdapter = new GalleryAdapter();
+
+            final File imagesDir = new File(String.valueOf(Environment.
+                    getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)));
+            final File[] files = imagesDir.listFiles();
+            final int filesCount = files.length;
+
+            final List<String> filesList = new ArrayList<>();
+            for (int i = 0; i < filesCount; i++) {
+                final String path = files[i].getAbsolutePath();
+                if (path.endsWith(".jpg") || path.endsWith(".png")) {
+                    filesList.add(path);
+                }
+            }
+
+            galleryAdapter.setData(filesList);
+            listView.setAdapter(galleryAdapter);
+
+            isGalleryInitialized = true;
+        }
+    }
+
+    final class GalleryAdapter extends BaseAdapter {
+        private List<String> data = new ArrayList<>();
+
+        void setData(List<String> data) {
+            if (this.data.size() > 0) {
+                data.clear();
+            }
+
+            this.data.addAll(data);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final ImageView imageView;
+
+            if (convertView == null) {
+                imageView = (ImageView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            Glide.with(MainActivity.this).load(data.get(position)).centerCrop().into(imageView);
+
+            return imageView;
         }
     }
 }
