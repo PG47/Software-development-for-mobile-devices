46f579bc85bcc51a2fe40c5dfaaf56043d1cf18a
PG47 <86549123+PG47@users.noreply.github.com>
2024-03-14 11:18:15 +0700

    update permission

--- Gallery/app/src/main/java/com/example/gallery/MainActivity.java
+++ Gallery/app/src/main/java/com/example/gallery/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.gallery;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.Manifest;
 import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,6 +28,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -33,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
     FragmentTransaction ft;
     fragmentTop_headbar f_headbar;
     fragmentBot_bottombar f_botbar;
+    private static final String tag = "PERMISSION_TAG";
+    private static final int REQUEST_PERMISSIONS = 1234;
+    private static final String [] PERMISSIONS = {
+            Manifest.permission.READ_MEDIA_IMAGES
+            ,Manifest.permission.MANAGE_EXTERNAL_STORAGE
+    };
+    private static final int PERMISSION_COUNT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,47 +60,90 @@ public class MainActivity extends AppCompatActivity {
         f_headbar = fragmentTop_headbar.newInstance("first-blue");
         f_botbar = fragmentBot_bottombar.newInstance("first-red");
 
-        // Begin a fragment transaction for FragmentLeft
         ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.headbar, f_headbar);
         ft.addToBackStack(null); // Add transaction to the back stack
         ft.commit();
 
-        // Begin another fragment transaction for fragmentBot_bottombar
         ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.botom_bar, f_botbar);
         ft.addToBackStack(null); // Add transaction to the back stack
         ft.commit();
-    }
 
-    private static final int REQUEST_PERMISSIONS = 1234;
-    private static final String [] PERMISSIONS = {
-            Manifest.permission.READ_MEDIA_IMAGES
-            //,Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
-
-    private static final int PERMISSION_COUNT = 1;
-
-    @SuppressLint("NewApi")
-    private boolean Permission_denied() {
-        for(int i=0; i < PERMISSION_COUNT; i++) {
-            if(checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED)
-                return true;
+        if(checkPermission()) {
+            //do later
+        } else {
+            requestPermission();
         }
-        return false;
+
     }
 
-    @SuppressLint("NewApi")
-    @Override
-    public void onRequestPermissionsResult(final int requestCode, final String[] Permissions, final int[] results) {
-        super.onRequestPermissionsResult(requestCode,Permissions,results);
-        if(requestCode == REQUEST_PERMISSIONS && results.length > 0) {
-            if(Permission_denied()) {
-                ((ActivityManager) Objects.requireNonNull(this.getSystemService(ACTIVITY_SERVICE)))
-                        .clearApplicationUserData();
-                recreate();
-            } else {
-                onResume();
+    private void requestPermission() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                Log.d(tag, "request permisison: try...");
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+                intent.setData(uri);
+                storageActivityResultLauncher.launch(intent);
+            } catch (Exception e) {
+                Log.d(tag, "request permisison: catch", e);
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                storageActivityResultLauncher.launch(intent);
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS,
+                    REQUEST_PERMISSIONS
+            );
+        }
+    }
+
+    public boolean checkPermission() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return Environment.isExternalStorageManager();
+        } else {
+            int write = ContextCompat.checkSelfPermission(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE);
+            int read = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES);
+            return write == PackageManager.PERMISSION_GRANTED && read == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+
+    private ActivityResultLauncher<Intent> storageActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult o) {
+                    if(Build.VERSION.SDK_INT > Build.VERSION_CODES.R) {
+                        if(Environment.isExternalStorageManager()) {
+                            Log.d(tag,"onActivityResult: Manage external Storage Permission is granted!");
+                        } else {
+                            Log.d(tag,"onActivityResult: Manage external Storage Permission is denied!");
+                            Toast.makeText(MainActivity.this,"Manage external Storage Permission is denied!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+
+                    }
+                }
+            }
+    );
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d("PermissionDebug", "onRequestPermissionsResult triggered");
+        if (requestCode  == REQUEST_PERMISSIONS) {
+            if(grantResults.length > 0) {
+                boolean write = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean read = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if(write && read) {
+                    Log.d(tag,"onActivityResult: External Storage Permission is granted!");
+                } else {
+                    Log.d(tag,"onActivityResult: External Storage Permission is denied!");
+                    Toast.makeText(MainActivity.this,"External Storage Permission is denied!", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     }
@@ -92,10 +153,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Permission_denied()) {
-            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
-            return;
-        }
+
 
         if (!isGalleryInitialized) {
             final ListView listView = findViewById(R.id.Pic_list);
