de29722406963a0f3f829c7b3d8d07baf773dc66
PG47 <86549123+PG47@users.noreply.github.com>
2024-03-28 15:26:42 +0700

    Revert "Up btvn_06"
    This reverts commit 4705622223b4b653dd419ee39d65387e64e866ae.

--- BTVN/btvn_06/app/src/main/java/com/example/w5_exercise/FragmentCallbacks.java
+++
@@ -1,5 +0,0 @@
-    package com.example.w5_exercise;
-
-    public interface FragmentCallbacks {
-        public void onMsgFromMainToFragment(String strValue);
-    }
--- BTVN/btvn_06/app/src/main/java/com/example/w5_exercise/MainCallbacks.java
+++
@@ -1,5 +0,0 @@
-package com.example.w5_exercise;
-
-public interface MainCallbacks {
-    public void onMsgFromFragToMain (String sender, String strValue);
-}
