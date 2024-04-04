1e4e0457169335550834e4976d392529ecc6c090
tongtaidatmai <103758260+5crylic@users.noreply.github.com>
2024-04-04 14:49:02 +0700

    delete selected image

--- Gallery/app/src/main/java/com/example/gallery/OptionFragment.java
+++ Gallery/app/src/main/java/com/example/gallery/OptionFragment.java
@@ -1,9 +1,16 @@
 package com.example.gallery;
 
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Path;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,9 +23,13 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class OptionFragment extends Fragment {
     DetailsActivity mainActivity;
     BottomNavigationView bottomOptionView;
+
+    private ArrayList<String> images;
 
     public static OptionFragment newInstance(String strArg1) {
         OptionFragment fragment = new OptionFragment();
@@ -66,7 +77,7 @@ public class OptionFragment extends Fragment {
 
                 return true;
             } else if (itemId == R.id.delete) {
-
+                confirmDeleteSelectedImage(selectedImage);
                 return true;
             }
 
@@ -74,5 +85,50 @@ public class OptionFragment extends Fragment {
         });
 
         return layoutOption;
+    }
+
+    private void confirmDeleteSelectedImage(String selectedImage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Confirm Delete");
+        builder.setMessage("Are you sure you want to delete this image?");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                deleteSelectedImage(selectedImage);
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
+    }
+
+    private void deleteSelectedImage(String selectedImage) {
+        ContentResolver contentResolver = requireActivity().getContentResolver();
+
+        // Xác định đường dẫn của ảnh cần xóa
+        String[] projection = {MediaStore.Images.Media._ID};
+        String selection = MediaStore.Images.Media.DATA + " = ?";
+        String[] selectionArgs = {selectedImage};
+        Uri queryUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        // Thực hiện truy vấn để lấy ID của ảnh cần xóa
+        Cursor cursor = contentResolver.query(queryUri, projection, selection, selectionArgs, null);
+        if (cursor != null) {
+            try {
+                int column_index_data = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID);
+                if (cursor.moveToFirst()) {
+                    long id = cursor.getLong(column_index_data);
+                    Uri deleteUri = ContentUris.withAppendedId(queryUri, id);
+
+                    // Xóa ảnh từ cơ sở dữ liệu media
+                    contentResolver.delete(deleteUri, null, null);
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        /*
+        images = getAllShownImagesPath(requireContext());
+        notifyDataSetChanged();
+        */
     }
 }
