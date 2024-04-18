b22accbd8ba27c5dc0fa1bea5435944b668ce443
PG47 <86549123+PG47@users.noreply.github.com>
2024-04-19 00:20:50 +0700

    fix onBackPressed for album

--- Gallery/app/build.gradle.kts
+++ Gallery/app/build.gradle.kts
@@ -52,4 +52,6 @@ dependencies {
     implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
     implementation("com.squareup.okhttp3:okhttp:3.10.0")
+
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
 }
