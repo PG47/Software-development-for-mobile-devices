a0b46aedbf4ad5cdd5fe81214db47f45afe03de3
PG47 <86549123+PG47@users.noreply.github.com>
2024-04-26 00:38:57 +0700

    reduce size

--- Gallery/OpenCC/build/intermediates/cxx/Debug/5e535d1l/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++ Gallery/OpenCC/build/intermediates/cxx/Debug/5e535d1l/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
-"C:\\Users\\bibo2\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+"C:\\Users\\ASUS\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "D:\\GitHub\\Software-development-for-mobile-devices\\Gallery\\OpenCC\\.cxx\\Debug\\5e535d1l\\arm64-v8a" ^
+  "C:\\Users\\ASUS\\Documents\\mobile app dev\\Software-development-for-mobile-devices\\Gallery\\OpenCC\\.cxx\\Debug\\5e535d1l\\arm64-v8a" ^
   opencv_jni_shared
