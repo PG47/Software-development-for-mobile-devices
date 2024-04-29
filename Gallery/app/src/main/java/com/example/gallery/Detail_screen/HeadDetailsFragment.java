83c50279e318da83ffce064e59096294a456c201
21127154 <henryquoccao@gmail.com>
2024-04-29 20:54:55 +0700

    add: set image as wallpaper

--- Gallery/app/src/main/java/com/example/gallery/Detail_screen/HeadDetailsFragment.java
+++ Gallery/app/src/main/java/com/example/gallery/Detail_screen/HeadDetailsFragment.java
@@ -1,22 +1,34 @@
 package com.example.gallery.Detail_screen;
 
+import android.app.WallpaperManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.gallery.R;
 
+import java.io.IOException;
+
 public class HeadDetailsFragment extends Fragment {
     DetailsActivity detailsActivity;
     Context context = null;
     ImageButton exit, addTag, addToAlbum, setWallpaper, advancedOption;
+    String selectedImage;
+    Bitmap originalBitmap;
 
     public static HeadDetailsFragment newInstance(String strArg) {
         HeadDetailsFragment fragment = new HeadDetailsFragment();
@@ -62,6 +74,9 @@ public class HeadDetailsFragment extends Fragment {
             }
         }
 
+        selectedImage = getArguments().getString("selectedImage");
+        originalBitmap = BitmapFactory.decodeFile(selectedImage);
+
         exit = (ImageButton) layoutImage.findViewById(R.id.getBackButton);
         addTag = (ImageButton) layoutImage.findViewById(R.id.component1);
         addToAlbum = (ImageButton) layoutImage.findViewById(R.id.component2);
@@ -90,7 +105,28 @@ public class HeadDetailsFragment extends Fragment {
         setWallpaper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                LayoutInflater inflater = LayoutInflater.from(context);
+                View view1 = inflater.inflate(R.layout.dialog_wallpaper_confirmation, null);
+                ImageView imageView = view1.findViewById(R.id.imageView);
+                imageView.setImageBitmap(originalBitmap);
+                builder.setView(view1);
+                builder.setTitle("Set Wallpaper");
+                builder.setMessage("Do you want to set this image as your wallpaper?");
+                builder.setPositiveButton("Set Wallpaper", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
+                        try {
+                            wallpaperManager.setBitmap(originalBitmap, null, true, WallpaperManager.FLAG_SYSTEM);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
         advancedOption.setOnClickListener(new View.OnClickListener() {
